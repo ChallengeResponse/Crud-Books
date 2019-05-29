5a2221a1a73e695a40c064eb824e6af212dd497f
Beng <ChallengeResponse>
2019-05-29 15:26:10 -0600

    Write Main.go
    Change make.go from a proof of db connectivity to a web server utilizing the app code

--- cmd/main.go
+++ cmd/main.go
@@ -1,46 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 	"crudBooks/internal/pkg/datastore"
+	"crudBooks/internal/pkg/router"
+	"crudBooks/internal/app/book"
 )
 
 func main(){
-/*
-	POST /books
-    GET /books
-    GET /books/{id}
-	PUT /books/{id}
-	PATCH /books/{id}
-	DELETE /books/{id}
+	/*
+		POST /books
+		GET /books
+		GET /books/{id}
+		PUT /books/{id}
+		PATCH /books/{id}
+		DELETE /books/{id}
 
-	where a book has:
-	- Title
-	- Author
-	- Publisher
-	- Publish Date
-	- Rating (1-3)
-	- Status (CheckedIn, CheckedOut)
-*/
-	var tableName string
+		where a book has:
+		- Title
+		- Author
+		- Publisher
+		- Publish Date
+		- Rating (1-3)
+		- Status (CheckedIn, CheckedOut)
+	*/
 	// container parent address = host.docker.internal
 	db := datastore.Main("127.0.0.1")
 	defer db.Close()
-	rows, err := db.Query("show tables")
-	if err != nil {
-		panic(err.Error())
-	}
-	defer rows.Close()
-	for rows.Next() {
-		err := rows.Scan(&tableName)
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Println(tableName)
-	}
-	err = rows.Err()
-	if err != nil {
-		panic(err.Error())
-	}
-	
+	path := "/books/"
+	var collection book.RestBooksStore
+
+	http.HandleFunc(path, router.RestFulSplitter(path, db, collection))
+    log.Fatal(http.ListenAndServe(":8080", nil))	
 }
